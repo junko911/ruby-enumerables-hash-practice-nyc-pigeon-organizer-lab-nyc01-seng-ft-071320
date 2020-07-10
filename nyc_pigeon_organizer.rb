1c7de1233f6b868600b8ef12741021218c856461
Junko Tahara <junkotahara911@gmail.com>
2020-07-10 03:34:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,42 +1,56 @@
 require 'pry'
 
-def attribute_of_the_pigeon(data, attribute, pigeon)
-  attribute_list = []
-  
-  data[attribute].each { |attribute_key, names|
-    
-    names.each { |name| 
-      if name == pigeon
-        attribute_list << attribute_key.to_s
-      end
-    }
-    
-  }
-  attribute_list
-end
-
-
-def attribute(data, pigeon)
-  attribute_lists = {}
-  
-  data.each_key { |attribute|
-    attribute_lists[attribute] = attribute_of_the_pigeon(data, attribute, pigeon)
-  }
-  
-  attribute_lists
-end
-
 def nyc_pigeon_organizer(data)
   new_list = {}
   
-  data[:gender].each_value { |names| 
-    names.each { |name|
-      new_list[name] = attribute(data, name)
-    }
-  }
+  data
   
-  new_list
 end
+
+
+
+
+
+
+
+
+# def attribute_of_the_pigeon(data, attribute, pigeon)
+#   attribute_list = []
+  
+#   data[attribute].each { |attribute_key, names|
+    
+#     names.each { |name| 
+#       if name == pigeon
+#         attribute_list << attribute_key.to_s
+#       end
+#     }
+    
+#   }
+#   attribute_list
+# end
+
+
+# def attribute(data, pigeon)
+#   attribute_lists = {}
+  
+#   data.each_key { |attribute|
+#     attribute_lists[attribute] = attribute_of_the_pigeon(data, attribute, pigeon)
+#   }
+  
+#   attribute_lists
+# end
+
+# def nyc_pigeon_organizer(data)
+#   new_list = {}
+  
+#   data[:gender].each_value { |names| 
+#     names.each { |name|
+#       new_list[name] = attribute(data, name)
+#     }
+#   }
+  
+#   new_list
+# end
 
 
 
